90f6f9387f5f7fd4a8702897a725db47c95115f9
SilverMoonDev <silvermoondev.contact@gmail.com>
2025-01-13 05:40:35 +0100

    Add project files.

---
+++ MeshToggler.cs
@@ -0,0 +1,90 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace Mesh_Toggler
+{
+    public class MeshToggler : MelonMod
+    {
+        private int characterID;
+        private Transform rootTransform;
+        private GameObject character;
+        private bool isUIPanelVisible = false;
+        private SkinnedMeshRenderer[] meshRenderers;
+
+        public override void OnLateUpdate()
+        {
+            if (rootTransform == null || rootTransform.childCount == 0) return;
+
+            var newCharacter = rootTransform.GetChild(0).gameObject;
+            if (newCharacter.GetInstanceID() != characterID)
+            {
+                AvatarChange(newCharacter);
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                isUIPanelVisible = !isUIPanelVisible;
+            }
+        }
+
+        private void AvatarChange(GameObject avatar)
+        {
+            if (avatar == null) return;
+
+            MelonLogger.Msg("Avatar Changed");
+            characterID = avatar.GetInstanceID();
+            character = avatar;
+            UpdateMeshRenderers();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            if (sceneName == "Main" && rootTransform == null)
+            {
+                rootTransform = GameObject.Find("CharactersRoot")?.transform;
+            }
+        }
+
+        private void UpdateMeshRenderers()
+        {
+            if (character != null)
+            {
+                meshRenderers = character.GetComponentsInChildren<SkinnedMeshRenderer>();
+            }
+        }
+
+        public override void OnGUI()
+        {
+            if (!isUIPanelVisible || meshRenderers == null || meshRenderers.Length == 0) return;
+
+            GUILayout.BeginArea(new Rect(300, 500, 220, 400), "Mesh Toggler", GUI.skin.window);
+
+            foreach (var renderer in meshRenderers)
+            {
+                bool isVisible = renderer.gameObject.activeSelf;
+                Color buttonColor = isVisible ? Color.green : Color.red;
+
+                GUI.skin.button.normal.textColor = buttonColor;
+                GUI.skin.button.hover.textColor = buttonColor;
+
+                if (GUILayout.Button($"Toggle {renderer.gameObject.name}"))
+                {
+                    ToggleMeshVisibility(renderer);
+                }
+            }
+
+            GUILayout.EndArea();
+        }
+
+        private void ToggleMeshVisibility(SkinnedMeshRenderer renderer)
+        {
+            if (renderer != null)
+            {
+                renderer.gameObject.SetActive(!renderer.gameObject.activeSelf);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using MelonLoader;
+using Mesh_Toggler;
+
+[assembly: MelonInfo(typeof(MeshToggler), "Mesh Toggler", "1.0", "SilverMoonDev")]
+[assembly: MelonGame("infiniteloop", "DesktopMate")]
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Mesh Toggler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mesh Toggler")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3c87dbb8-a9bd-4beb-b83e-f4a1a9872377")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
