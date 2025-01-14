9dfc62b13778967e4a67664a1eb38d94b08d6a48
SilverMoonDev <silvermoondev.contact@gmail.com>
2025-01-14 13:25:21 +0100

    New Feature So It Saves The Clothes/Meshes States (It only works with Custom Avatars)

--- MeshToggler.cs
+++ MeshToggler.cs
@@ -1,5 +1,9 @@
 ﻿using MelonLoader;
 using UnityEngine;
+using System.IO;
+using System.Linq;
+using MelonLoader.Utils;
+using Mesh_Toggler.Patches;
 
 namespace Mesh_Toggler
 {
@@ -10,6 +14,23 @@ namespace Mesh_Toggler
         private GameObject character;
         private bool isUIPanelVisible = false;
         private SkinnedMeshRenderer[] meshRenderers;
+        private string avatarName;
+
+        private string iniFilePath;
+        private string configFilePath = Path.Combine(MelonEnvironment.UserDataDirectory, "MelonPreferences.cfg");
+
+        private IniFile iniFile;
+
+        public override void OnInitializeMelon()
+        {
+            PatchManager.InitPatch();
+
+            iniFilePath = Path.Combine(MelonEnvironment.UserDataDirectory, "MeshToggler.ini");
+
+            iniFile = new IniFile(iniFilePath);
+
+            LoadMeshStates();
+        }
 
         public override void OnLateUpdate()
         {
@@ -38,6 +59,11 @@ namespace Mesh_Toggler
             characterID = avatar.GetInstanceID();
             character = avatar;
             UpdateMeshRenderers();
+
+            if (PatchManager.defaultAvatar) return;
+
+            avatarName = GetAvatarNameFromConfig();
+            LoadMeshStates();
         }
 
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
@@ -61,7 +87,6 @@ namespace Mesh_Toggler
             if (!isUIPanelVisible || meshRenderers == null || meshRenderers.Length == 0) return;
 
             int numButtons = meshRenderers.Length;
-
             float UIHeight = (numButtons * 26f) + 15f;
 
             GUILayout.BeginArea(new Rect(300, 500, 220, UIHeight), "Mesh Toggler", GUI.skin.window);
@@ -88,7 +113,56 @@ namespace Mesh_Toggler
             if (renderer != null)
             {
                 renderer.gameObject.SetActive(!renderer.gameObject.activeSelf);
+                SaveMeshStates();
             }
+        }
+
+        private void SaveMeshStates()
+        {
+            if (PatchManager.defaultAvatar) return;
+            if (meshRenderers == null || meshRenderers.Length == 0) return;
+
+            string states = string.Join(",", meshRenderers.Select(renderer => renderer.gameObject.activeSelf ? "1" : "0"));
+
+            iniFile.SetString(avatarName, "MeshStates", states);
+        }
+
+        private void LoadMeshStates()
+        {
+            if (meshRenderers == null || meshRenderers.Length == 0 || avatarName == null) return;
+
+            string savedStates = iniFile.GetString(avatarName, "MeshStates", string.Empty);
+
+            if (string.IsNullOrEmpty(savedStates)) return;
+
+            string[] states = savedStates.Split(',');
+
+            for (int i = 0; i < meshRenderers.Length && i < states.Length; i++)
+            {
+                meshRenderers[i].gameObject.SetActive(states[i] == "1");
+            }
+        }
+
+        private string GetAvatarNameFromConfig()
+        {
+            if (!File.Exists(configFilePath))
+            {
+                MelonLogger.Error("Config file not found!");
+                return string.Empty;
+            }
+
+            var lines = File.ReadAllLines(configFilePath);
+            foreach (var line in lines)
+            {
+                if (line.StartsWith("vrmPath"))
+                {
+                    string vrmPath = line.Split('=')[1].Trim().Trim('\'');
+
+                    return Path.GetFileNameWithoutExtension(vrmPath);
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
---
+++ Patches/PatchManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mesh_Toggler.Patches
+{
+    internal class PatchManager
+    {
+        public static HarmonyLib.Harmony Instance = new HarmonyLib.Harmony("MeshToggler");
+
+        public static bool defaultAvatar = true;
+        private static readonly string DefaultAvatarsPath = "/MenuCanvas/MenuParent/ModelPage/Scroll View/Viewport/Content";
+
+        private static HarmonyMethod GetPatch(string name)
+        {
+            return new HarmonyMethod(typeof(PatchManager).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic));
+        }
+
+        public static unsafe void InitPatch()
+        {
+            try
+            {
+                var loadCharacterMethod = typeof(MelonLoaderMod1.Core).GetMethod("LoadCharacter", BindingFlags.Instance | BindingFlags.Public);
+
+                if (loadCharacterMethod != null) {
+                    Instance.Patch(loadCharacterMethod, GetPatch("OnCustomAvatarLoading"), null);
+                }
+                Instance.Patch(typeof(Button).GetMethod("Press"), GetPatch("OnButtonClicked"), null);
+            }
+            catch (Exception arg)
+            {
+                MelonLogger.Msg(string.Format("Failed Patching\n{0}", arg, ConsoleColor.Green));
+            }
+        }
+
+
+        private static void OnCustomAvatarLoading()
+        {
+            defaultAvatar = false;
+        }
+
+
+        private static void OnButtonClicked(Button __instance)
+        {
+            var path = GetPath(__instance.transform.parent.gameObject);
+            if (path == DefaultAvatarsPath)
+            {
+                if (__instance.interactable)
+                    defaultAvatar = true;
+            }
+        }
+
+        private static string GetPath(GameObject gameObject)
+        {
+            var path = "/" + gameObject.name;
+            while (gameObject.transform.parent != null)
+            {
+                gameObject = gameObject.transform.parent.gameObject;
+                path = "/" + gameObject.name + path;
+            }
+            return path;
+        }
+    }
+}
